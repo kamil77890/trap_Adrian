90babd735a503de063cd2b43bcdeccda8bdfa76f
Kamil Jakubisiak <u87_kamjak_waw@technischools.com>
2024-02-20 18:56:27 +0100

    fg

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <div>
-        <small>Thats a trap bro...</small>
+        <small>Thats a trap...</small>
         <FaFaceSmileWink />
       </div>
     </>
